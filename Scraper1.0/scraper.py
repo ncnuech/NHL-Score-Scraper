e0f88e6e9e805fe8dc7f08f63dd7c1e87d4cc5d1
ncnuech <ncnuech@umich.edu>
2016-09-25 14:54:56 -0500

    created league object to hold both abbreviations for printing as well as filepaths for buzzers

--- Scraper1.0/scraper.py
+++ Scraper1.0/scraper.py
@@ -31,6 +31,8 @@ class Game:
 	gameStarted = False;
 	#Boolean for whether game has ended True if ended (unimplemented)
 	gameEnded = False;
+	#string for type of game status used for end game right now
+	gameStatusStr = "";
 
 	#id used to identify a given game(most notibly used by ESPN.com)
 	gameId = 0;
@@ -71,7 +73,47 @@ class Game:
 			print("no score change")
 			return False
 
+class League:
 
+	teamDict = {}
+
+	def setupTeamDict(self):
+
+		prefix = "BuzzerSounds/"
+		self.teamDict["Blackhawks"] = {"abbr":"CHI","buzzerFile":prefix+ "chicago.mp3"}
+		self.teamDict["Avalanche"] = {"abbr":"COL","buzzerFile":prefix + "colorado.mp3"}
+		self.teamDict["Stars"] = {"abbr":"DAL","buzzerFile": prefix + "dallas.mp3"}
+		self.teamDict["Wild"] ={"abbr":"MIN","buzzerFile":prefix + "minnesota.mp3"}
+		self.teamDict["Predators"] = {"abbr":"NSH","buzzerFile":prefix + "nashville.mp3"}
+		self.teamDict["Blues"] = {"abbr":"STL","buzzerFile":prefix + "stlouis.mp3"}
+		self.teamDict["Jets"] = {"abbr":"WIN","buzzerFile":prefix + "winnepeg.mp3"}
+		self.teamDict["Bruins"] = {"abbr":"BOS","buzzerFile":prefix+"boston.mp3"}
+		self.teamDict["Sabres"] = {"abbr":"BUF","buzzerFile":prefix+"buffalo.mp3"}
+		self.teamDict["Red Wings"] = {"abbr":"DET","buzzerFile":prefix+ "detroit.mp3"}
+		self.teamDict["Panthers"] = {"abbr":"FLA","buzzerFile":prefix+"florida.mp3"}
+		self.teamDict["Canadiens"] = {"abbr":"MTL","buzzerFile":prefix+"montreal.mp3"}
+		self.teamDict["Senators"] = {"abbr":"OTT","buzzerFile":prefix+"ottawa.mp3"}
+		self.teamDict["Lightning"] = {"abbr":"TB","buzzerFile":prefix+"tampabay.mp3"}
+		self.teamDict["Maple Leafs"] = {"abbr":"TOR","buzzerFile":prefix+"toronto.mp3"}
+		self.teamDict["Ducks"] = {"abbr":"ANA","buzzerFile":prefix+"anaheim.mp3"}
+		self.teamDict["Coyotes"] = {"abbr":"ARI","buzzerFile":prefix+ "arizona.mp3"}
+		self.teamDict["Flames"] = {"abbr":"CGY","buzzerFile":prefix+ "calgary.mp3"}
+		self.teamDict["Oilers"] = {"abbr":"EDM","buzzerFile":prefix+ "edmonton.mp3"}
+		self.teamDict["Kings"] = {"abbr":"LA","buzzerFile":prefix+"losangeles.mp3"}
+		self.teamDict["Sharks"] = {"abbr":"SJ","buzzerFile":prefix+"sanjose.mp3"}
+		self.teamDict["Canucks"] = {"abbr":"VAN","buzzerFile":prefix+"vancouver.mp3"}
+		self.teamDict["Hurricanes"] = {"abbr":"CAR","buzzerFile":prefix+"carolina.mp3"}
+		self.teamDict["Blue Jackets"] = {"abbr":"CLS","buzzerFile":prefix+"columbus.mp3"}
+		self.teamDict["Devils"] = {"abbr":"NJ","buzzerFile":prefix+"newjersey.mp3"}
+		self.teamDict["Islanders"] = {"abbr":"NYI","buzzerFile":prefix+"newyorkislanders.mp3"}
+		self.teamDict["Rangers"] = {"abbr":"NYR","buzzerFile":prefix+"newyorkrangers.mp3"}
+		self.teamDict["Flyers"] = {"abbr":"PHI","buzzerFile":prefix+"philadelphia.mp3"}
+		self.teamDict["Penguins"] = {"abbr":"PIT","buzzerFile":prefix+"pittsburgh.mp3"}
+		self.teamDict["Capitals"] = {"abbr":"WSH","buzzerFile":prefix+"washington.mp3"}
+
+
+	def __init__(self):
+		self.setupTeamDict()
 ###############################################
 #Code to play sound (unfinished)
 #threading starts a 2nd thread in a callback function
@@ -79,52 +121,13 @@ class Game:
 #call subprocess with vlc then shut down after x seconds
 class Buzzer:
 
-	#Current dictionary to hold team name to location of mp3 file for buzzer
-	buzzerDict = {}
-
-
-	#hardcoded paths to mp3 files
-	#may make dict to teams and just use that in future
-	def setupBuzzerDict(self):
-		prefix = "BuzzerSounds/"
-		self.buzzerDict["Blackhawks"] = prefix + "chicago.mp3"
-		self.buzzerDict["Avalanche"] = prefix + "colorado.mp3"
-		self.buzzerDict["Stars"] = prefix + "dallas.mp3"
-		self.buzzerDict["Wild"] = prefix + "minnesota.mp3"
-		self.buzzerDict["Predators"] = prefix + "nashville.mp3"
-		self.buzzerDict["Blues"] = prefix + "stlouis.mp3"
-		self.buzzerDict["Jets"] = prefix + "winnepeg.mp3"
-		self.buzzerDict["Bruins"] = prefix + "boston.mp3"
-		self.buzzerDict["Sabres"] = prefix + "buffalo.mp3"
-		self.buzzerDict["Red Wings"] = prefix + "detroit.mp3"
-		self.buzzerDict["Panthers"] = prefix + "florida.mp3"
-		self.buzzerDict["Canadiens"] = prefix + "montreal.mp3"
-		self.buzzerDict["Senators"] = prefix + "ottawa.mp3"
-		self.buzzerDict["Lightning"] = prefix + "tampabay.mp3"
-		self.buzzerDict["Maple Leafs"] = prefix + "toronto.mp3"
-		self.buzzerDict["Ducks"] = prefix + "anaheim.mp3"
-		self.buzzerDict["Coyotes"] = prefix + "arizona.mp3"
-		self.buzzerDict["Flames"] = prefix + "calgary.mp3"
-		self.buzzerDict["Oilers"] = prefix + "edmonton.mp3"
-		self.buzzerDict["Kings"] = prefix + "losangeles.mp3"
-		self.buzzerDict["Sharks"] = prefix + "sanjose.mp3"
-		self.buzzerDict["Canucks"] = prefix + "vancouver.mp3"
-		self.buzzerDict["Hurricanes"] = prefix + "carolina.mp3"
-		self.buzzerDict["Blue Jackets"] = prefix + "columbus.mp3"
-		self.buzzerDict["Devils"] = prefix + "newjersey.mp3"
-		self.buzzerDict["Islanders"] = prefix + "newyorkislanders.mp3"
-		self.buzzerDict["Rangers"] = prefix + "newyorkrangers.mp3"
-		self.buzzerDict["Flyers"] = prefix + "philadelphia.mp3"
-		self.buzzerDict["Penguins"] = prefix + "pittsburgh.mp3"
-		self.buzzerDict["Capitals"] = prefix + "washington.mp3"
-
 	#starts the buzzer when a specific team scores. must call with team name
 	#uses vlc player right now. will have to work if go to rPI
 	#currently 10 seconds to end buzzer
 	#buzzers will play on top of each other
 	def startBuzzer(self,teamName):
 		sound_player = "C:/Program Files (x86)/VideoLAN/VLC/vlc.exe"
-		sound_file = self.buzzerDict[teamName]
+		sound_file = leagueObj.teamDict[teamName]['buzzerFile']
 		music_player_subprocess = subprocess.Popen([sound_player,sound_file])
 		threading.Timer(10.0,self.endBuzzer,[music_player_subprocess]).start()
 
@@ -133,8 +136,7 @@ class Buzzer:
 		os.kill(music_player_subprocess.pid, signal.SIGINT)
 
 	def __init__(self):		
-		#threading.Timer(.1,self.startBuzzer).start()
-		self.setupBuzzerDict()
+		return
 ###############################################
 
 class ESPNSportsObj:
@@ -146,8 +148,7 @@ class ESPNSportsObj:
 
 	def __init__(self):
 		#Retrieve the HTML for ESPN scoreboard
-		dateStr = getDateStr()
-		page = requests.get('http://espn.go.com/nhl/scoreboard?date='+dateStr)
+		page = requests.get('http://espn.go.com/nhl/scoreboard?date='+getDateStr())
 		tree = html.fromstring(page.content);
 
 		#Retrieve ids for each game on the current day.
@@ -176,18 +177,27 @@ class ESPNSportsObj:
 	#load the current score of all games of the day and compare to previous values
 	def loadScoreboard(self):
 		#Retreive HTML for ESPN Scoreboard
-		dateStr = getDateStr()
-		page = requests.get('http://espn.go.com/nhl/scoreboard?date='+dateStr)
+		page = requests.get('http://espn.go.com/nhl/scoreboard?date='+getDateStr())
 		tree = html.fromstring(page.content);
 
-		#For each game check score vs previous
+		#For each game check score vs previous as well as game status
 		for game in self.gameList:
-			
+			if (game.gameEnded):
+			    continue
 			homeScore = tree.xpath('//*[@id="' +  game.gameId +  '-homeHeaderScore"]/text()')[0]
-			if homeScore=='\xa0':
+			awayScore = tree.xpath('//*[@id="' +  game.gameId +  '-awayHeaderScore"]/text()')[0]
+			game.gameStatusStr=tree.xpath('//*[@id="'+ game.gameId + '-statusLine1"]/text()')[0]
+			if (homeScore=='\xa0'):
 				print("game has not started")
 				continue
-			awayScore = tree.xpath('//*[@id="' +  game.gameId +  '-awayHeaderScore"]/text()')[0]
+			elif (not game.gameStarted):
+				print("Game Just Started")
+				game.gameStarted=True
+			elif (not game.gameEnded and (game.gameStatusStr=='Final' or game.gameStatusStr=='Final/OT')):
+				print("Game Just Ended")
+				game.gameEnded = True
+				self.loadGame(game)
+				continue
 
 			#if score has changed, update score and send alert.
 			if  game.checkScore(int(awayScore),int(homeScore)):
@@ -201,16 +211,19 @@ class ESPNSportsObj:
 		#Retreive the boxscore HTML from ESPN for a game
 		page = requests.get(game.url)
 		tree = html.fromstring(page.content);
-
-		print(game.homeTeam + " " + str(game.homeScore) + " - " + game.awayTeam + " " + str(game.awayScore))
-		#Retreive list of scorers in order (not(@colspan) removes penalty plays)
-		playList =  tree.xpath('//*[@id="my-players-table"]/div[4]/div/table/*/*/td[3][not(@colspan)]/text()');
-		#Retreive list of assisters in order (0,1 or 2 can be given on a single line. 0 being unnasisted)
-		playList2 =  tree.xpath('//*[@id="my-players-table"]/div[4]/div/table/*/*/td[3]/i/text()');
-		#Concatonate last goal scorer and last assister and print this as the most recent scoring play
-		mostRecent = playList[-1] + playList2[-1]
-		print(mostRecent);
-
+		outputString = leagueObj.teamDict[game.homeTeam]['abbr'] + " " + str(game.homeScore) + " - " + leagueObj.teamDict[game.awayTeam]['abbr'] + " " + str(game.awayScore)
+		if (not game.gameEnded):
+			#Retreive list of scorers in order (not(@colspan) removes penalty plays)
+			playList =  tree.xpath('//*[@id="my-players-table"]/div[3]/div/table/*/*/td[3][not(@colspan)]/text()');
+			#Retreive list of assisters in order (0,1 or 2 can be given on a single line. 0 being unnasisted)
+			playList2 =  tree.xpath('//*[@id="my-players-table"]/div[3]/div/table/*/*/td[3]/i/text()');
+			#Concatonate last goal scorer and last assister and print this as the most recent scoring play
+			mostRecent = " " + playList[-1] + playList2[-1]
+			outputString+=mostRecent
+			print(mostRecent);
+		else:
+			outputString+=" " + game.gameStatusStr	
+		print(outputString)
 
 #retrive the headlines from NHL.com
 def getNHLHeadlines():
@@ -220,7 +233,7 @@ def getNHLHeadlines():
 	print(headlines)
 
 buzzerObj = Buzzer()
-
+leagueObj = League()
 def getDateStr():
 	dateStr = time.strftime("%Y%m%d") #oes this work for single digit days?
 	#dateStr="20160503"
