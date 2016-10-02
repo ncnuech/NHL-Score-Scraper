59fbbdb446665bf158de58ed8238e99141682664
ncnuech <ncnuech@umich.edu>
2016-10-02 11:58:09 -0500

    added color changes

--- Scraper1.0/scraper.py
+++ Scraper1.0/scraper.py
@@ -86,20 +86,15 @@ class Game:
 	def checkScore(self,newAwayScore,newHomeScore):
 		changed=0
 		if(newAwayScore != self.awayScore):
-			printerObj.debugPrint("away score changed")
 			self.awayScore = newAwayScore
-			#buzzerObj.startBuzzer(self.awayTeam)
 			changed=1
 			
 		if (newHomeScore != self.homeScore):
 			self.homeScore = newHomeScore
-			#buzzerObj.startBuzzer(self.homeTeam)
-			printerObj.debugPrint("home score changed")
 			changed=2
 			
-
-		printerObj.debugPrint("no score change")
 		return changed
+	
 	def getScoringTeamName(self,scoringTeam):
 		if (scoringTeam==1):
 			return self.awayTeam
@@ -112,37 +107,42 @@ class League:
 	def setupTeamDict(self):
 
 		prefix = "BuzzerSounds/"
-		self.teamDict["Blackhawks"] = {"abbr":"CHI","buzzerFile":prefix+ "chicago.mp3"}
-		self.teamDict["Avalanche"] = {"abbr":"COL","buzzerFile":prefix + "colorado.mp3"}
-		self.teamDict["Stars"] = {"abbr":"DAL","buzzerFile": prefix + "dallas.mp3"}
-		self.teamDict["Wild"] ={"abbr":"MIN","buzzerFile":prefix + "minnesota.mp3"}
-		self.teamDict["Predators"] = {"abbr":"NSH","buzzerFile":prefix + "nashville.mp3"}
-		self.teamDict["Blues"] = {"abbr":"STL","buzzerFile":prefix + "stlouis.mp3"}
-		self.teamDict["Jets"] = {"abbr":"WIN","buzzerFile":prefix + "winnepeg.mp3"}
-		self.teamDict["Bruins"] = {"abbr":"BOS","buzzerFile":prefix+"boston.mp3"}
-		self.teamDict["Sabres"] = {"abbr":"BUF","buzzerFile":prefix+"buffalo.mp3"}
-		self.teamDict["Red Wings"] = {"abbr":"DET","buzzerFile":prefix+ "detroit.mp3"}
-		self.teamDict["Panthers"] = {"abbr":"FLA","buzzerFile":prefix+"florida.mp3"}
-		self.teamDict["Canadiens"] = {"abbr":"MTL","buzzerFile":prefix+"montreal.mp3"}
-		self.teamDict["Senators"] = {"abbr":"OTT","buzzerFile":prefix+"ottawa.mp3"}
-		self.teamDict["Lightning"] = {"abbr":"TB","buzzerFile":prefix+"tampabay.mp3"}
-		self.teamDict["Maple Leafs"] = {"abbr":"TOR","buzzerFile":prefix+"toronto.mp3"}
-		self.teamDict["Ducks"] = {"abbr":"ANA","buzzerFile":prefix+"anaheim.mp3"}
-		self.teamDict["Coyotes"] = {"abbr":"ARI","buzzerFile":prefix+ "arizona.mp3"}
-		self.teamDict["Flames"] = {"abbr":"CGY","buzzerFile":prefix+ "calgary.mp3"}
-		self.teamDict["Oilers"] = {"abbr":"EDM","buzzerFile":prefix+ "edmonton.mp3"}
-		self.teamDict["Kings"] = {"abbr":"LA","buzzerFile":prefix+"losangeles.mp3"}
-		self.teamDict["Sharks"] = {"abbr":"SJ","buzzerFile":prefix+"sanjose.mp3"}
-		self.teamDict["Canucks"] = {"abbr":"VAN","buzzerFile":prefix+"vancouver.mp3"}
-		self.teamDict["Hurricanes"] = {"abbr":"CAR","buzzerFile":prefix+"carolina.mp3"}
-		self.teamDict["Blue Jackets"] = {"abbr":"CLS","buzzerFile":prefix+"columbus.mp3"}
-		self.teamDict["Devils"] = {"abbr":"NJ","buzzerFile":prefix+"newjersey.mp3"}
-		self.teamDict["Islanders"] = {"abbr":"NYI","buzzerFile":prefix+"newyorkislanders.mp3"}
-		self.teamDict["Rangers"] = {"abbr":"NYR","buzzerFile":prefix+"newyorkrangers.mp3"}
-		self.teamDict["Flyers"] = {"abbr":"PHI","buzzerFile":prefix+"philadelphia.mp3"}
-		self.teamDict["Penguins"] = {"abbr":"PIT","buzzerFile":prefix+"pittsburgh.mp3"}
-		self.teamDict["Capitals"] = {"abbr":"WSH","buzzerFile":prefix+"washington.mp3"}
+		self.teamDict["Blackhawks"] = {"abbr":"CHI","buzzerFile":prefix+ "chicago.mp3","hex":"#e41836"}
+		self.teamDict["Avalanche"] = {"abbr":"COL","buzzerFile":prefix + "colorado.mp3","hex":"#870038"}
+		self.teamDict["Stars"] = {"abbr":"DAL","buzzerFile": prefix + "dallas.mp3","hex":"#03613f"}
+		self.teamDict["Wild"] ={"abbr":"MIN","buzzerFile":prefix + "minnesota.mp3","hex":"#164734"}
+		self.teamDict["Predators"] = {"abbr":"NSH","buzzerFile":prefix + "nashville.mp3","hex":"#ffb71a"}
+		self.teamDict["Blues"] = {"abbr":"STL","buzzerFile":prefix + "stlouis.mp3","hex":"#083377"}
+		self.teamDict["Jets"] = {"abbr":"WIN","buzzerFile":prefix + "winnepeg.mp3","hex":"#002e62"}
+		self.teamDict["Bruins"] = {"abbr":"BOS","buzzerFile":prefix+"boston.mp3","hex":"#fcb930"}
+		self.teamDict["Sabres"] = {"abbr":"BUF","buzzerFile":prefix+"buffalo.mp3","hex":"#f2b434"}
+		self.teamDict["Red Wings"] = {"abbr":"DET","buzzerFile":prefix+ "detroit.mp3","hex":"#d42234"}
+		self.teamDict["Panthers"] = {"abbr":"FLA","buzzerFile":prefix+"florida.mp3","hex":"#c49818"}
+		self.teamDict["Canadiens"] = {"abbr":"MTL","buzzerFile":prefix+"montreal.mp3","hex":"#c51230"}
+		self.teamDict["Senators"] = {"abbr":"OTT","buzzerFile":prefix+"ottawa.mp3","hex":"#d4a00f"}
+		self.teamDict["Lightning"] = {"abbr":"TB","buzzerFile":prefix+"tampabay.mp3","hex":"#ffffff"}
+		self.teamDict["Maple Leafs"] = {"abbr":"TOR","buzzerFile":prefix+"toronto.mp3","hex":"#013e7f"}
+		self.teamDict["Ducks"] = {"abbr":"ANA","buzzerFile":prefix+"anaheim.mp3","hex":"#b6985a"}
+		self.teamDict["Coyotes"] = {"abbr":"ARI","buzzerFile":prefix+ "arizona.mp3","hex":"#98012e"}
+		self.teamDict["Flames"] = {"abbr":"CGY","buzzerFile":prefix+ "calgary.mp3","hex":"#cb0d0d"}
+		self.teamDict["Oilers"] = {"abbr":"EDM","buzzerFile":prefix+ "edmonton.mp3","hex":"#eb6e1e"}
+		self.teamDict["Kings"] = {"abbr":"LA","buzzerFile":prefix+"losangeles.mp3","hex":"#231f20"}
+		self.teamDict["Sharks"] = {"abbr":"SJ","buzzerFile":prefix+"sanjose.mp3","hex":"#006e76"}
+		self.teamDict["Canucks"] = {"abbr":"VAN","buzzerFile":prefix+"vancouver.mp3","hex":"#013e7f"}
+		self.teamDict["Hurricanes"] = {"abbr":"CAR","buzzerFile":prefix+"carolina.mp3","hex":"#da2e2a"}
+		self.teamDict["Blue Jackets"] = {"abbr":"CLS","buzzerFile":prefix+"columbus.mp3","hex":"#002e62"}
+		self.teamDict["Devils"] = {"abbr":"NJ","buzzerFile":prefix+"newjersey.mp3","hex":"#ff0000"}
+		self.teamDict["Islanders"] = {"abbr":"NYI","buzzerFile":prefix+"newyorkislanders.mp3","hex":"#f57d31"}
+		self.teamDict["Rangers"] = {"abbr":"NYR","buzzerFile":prefix+"newyorkrangers.mp3","hex":"#005dab"}
+		self.teamDict["Flyers"] = {"abbr":"PHI","buzzerFile":prefix+"philadelphia.mp3","hex":"#f37737"}
+		self.teamDict["Penguins"] = {"abbr":"PIT","buzzerFile":prefix+"pittsburgh.mp3","hex":"#7ed5fa"}
+		self.teamDict["Capitals"] = {"abbr":"WSH","buzzerFile":prefix+"washington.mp3","hex":"#e51837"}
+		self.teamDict["Default"] = {"abbr":"DEF","buzzerFile":prefix+"hartford.mp3","hex":"#fffe6"}
 
+
+	def getFormattedTeamString(self,teamName):
+		defaultBrightness="7"
+		return self.teamDict[teamName]['hex'] + defaultBrightness + self.teamDict[teamName]['abbr'] + self.teamDict['Default']['hex'] + defaultBrightness
 
 	def __init__(self):
 		self.setupTeamDict()
@@ -158,7 +158,10 @@ class Buzzer:
 	#currently 10 seconds to end buzzer
 	#buzzers will play on top of each other
 	def startBuzzer(self,teamName):
-		sound_player = "/usr/bin/cvlc"
+		if os.name=="nt":
+			sound_player = "C:/Program Files (x86)/VideoLAN/VLC/vlc.exe"
+		else:
+			sound_player = "/usr/bin/cvlc"
 		sound_file = leagueObj.teamDict[teamName]['buzzerFile']
 		music_player_subprocess = subprocess.Popen([sound_player,sound_file])
 		threading.Timer(10.0,self.endBuzzer,[music_player_subprocess]).start()
@@ -216,11 +219,11 @@ class ESPNSportsObj:
 		for game in self.gameList:
 			gameStr=""
 			if not game.gameStarted:
-				gameStr+=leagueObj.teamDict[game.homeTeam]['abbr'] + " vs " + leagueObj.teamDict[game.awayTeam]['abbr'] + " " + game.gameTime
+				gameStr+=leagueObj.getFormattedTeamString(game.homeTeam) + " vs " + leagueObj.getFormattedTeamString(game.awayTeam) + " " + game.gameTime
 			elif not game.gameEnded:
-				gameStr+=leagueObj.teamDict[game.homeTeam]['abbr']+ " " + str(game.homeScore) + " - " + leagueObj.teamDict[game.awayTeam]['abbr'] + " " + str(game.awayScore)
+				gameStr+=leagueObj.getFormattedTeamString(game.homeTeam) + " " + str(game.homeScore) + " - " + leagueObj.getFormattedTeamString(game.awayTeam) + " " + str(game.awayScore)
 			else:
-				gameStr+=leagueObj.teamDict[game.homeTeam]['abbr'] + " " + str(game.homeScore) + " - " + leagueObj.teamDict[game.awayTeam]['abbr'] + " " +  str(game.awayScore) +" " + game.gameStatusStr
+				gameStr+=leagueObj.getFormattedTeamString(game.homeTeam) + " " + str(game.homeScore) + " - " + leagueObj.getFormattedTeamString(game.awayTeam) + " " +  str(game.awayScore) +" " + game.gameStatusStr
 			gameStr+="   ";
 			leagueStr+=gameStr
 		return leagueStr
@@ -267,10 +270,11 @@ class ESPNSportsObj:
 		#Retreive the boxscore HTML from ESPN for a game
 		page = requests.get(game.url)
 		tree = html.fromstring(page.content);
-		outputString = leagueObj.teamDict[game.homeTeam]['abbr'] + " " + str(game.homeScore) + " - " + leagueObj.teamDict[game.awayTeam]['abbr'] + " " + str(game.awayScore) 
+		outputString = leagueObj.getFormattedTeamString(game.homeTeam) + " " + str(game.homeScore) + " - " + leagueObj.getFormattedTeamString(game.awayTeam) + " " + str(game.awayScore) 
 
 		if (not game.gameEnded):
-			outputString+= " " + leagueObj.teamDict[scoringTeamName]['abbr'] + " Goal! "
+			#buzzerObj.startBuzzer(scoringTeamName)
+			outputString+= " " + leagueObj.getFormattedTeamString(scoringTeamName) + " Goal! "
 			#Retreive list of scorers in order (not(@colspan) removes penalty plays) //which div
 			playList =  tree.xpath('//*[@id="my-players-table"]/*/div/table/*/*/td[3][not(@colspan)]/text()');
 			#Retreive list of assisters in order (0,1 or 2 can be given on a single line. 0 being unnasisted)
@@ -288,7 +292,7 @@ def getNHLHeadlines():
 	page = requests.get("https://www.nhl.com/")
 	tree = html.fromstring(page.content);
 	headlines = tree.xpath('//*[@id="content-wrap"]/div/div[3]/div[2]/section[1]/ul/*/a/text()')
-	printerObj.printToBoard(headlines)
+	printerObj.printToBoard(leagueObj.teamDict['Default']['hex'] + "7" + headlines)
 
 buzzerObj = Buzzer()
 leagueObj = League()
@@ -323,7 +327,7 @@ def main():
 	printerObj.debugPrint("ending")
 
 
-
+#Test function that sends request directly to board
 def testBoardCommunication():
 	prefix="http://10.177.105.74:81/text/"
 	prefix="http://10.177.105.137/arduino/ledText2/"
@@ -331,5 +335,4 @@ def testBoardCommunication():
 	print(rval)
 
 if __name__ == '__main__':
-	#testBoardCommunication()
 	main();
