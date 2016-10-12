3b83122bacde2b1a16b7a92189ac3b877f11f678
Noah Nuechterlein <ncnuech@umich.edu>
2016-10-12 19:12:20 -0400

    updating run script

--- Scraper1.0/run.sh
+++ Scraper1.0/run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 pwd
 ls
-sleep 45
+sleep 60
+cd Documents/NHL-Score-Scraper/Scraper1.0
+amixer -D pulse sset Master 100%
 /usr/bin/python /home/nhl/Documents/NHL-Score-Scraper/Scraper1.0/scraper.py &
